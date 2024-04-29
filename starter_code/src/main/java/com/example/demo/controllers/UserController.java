c72d7d51cc3267773509a9c00c9b51a7e37b66df
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-04-29 13:57:49 +0200

    added Logger to UserController

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -6,6 +6,8 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,6 +29,8 @@ public class UserController {
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
 
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class.getSimpleName());
+
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		return ResponseEntity.of(userRepository.findById(id));
@@ -46,14 +50,17 @@ public class UserController {
 		cartRepository.save(cart);
 		user.setCart(cart);
 
-		if (!createUserRequest.getPassword().equals( createUserRequest.getConfirmPassword())){throw new IllegalArgumentException("Password and Confirmed Password does not equal");}
-		else if(createUserRequest.getPassword() == null || createUserRequest.getPassword().length() < 7){throw new IllegalArgumentException("Password must have at least 7 Characters");}
-		else {
+		if (!createUserRequest.getPassword().equals( createUserRequest.getConfirmPassword())){
+			logger.warn("Create User " +createUserRequest.getUsername() +" failed, Password and confirmed Password are not equal!");
+			return ResponseEntity.badRequest().build();
+			}
+		else if(createUserRequest.getPassword() == null || createUserRequest.getPassword().length() < 7){
+			logger.warn("Create User " +createUserRequest.getUsername() +" failed. Password must have at least a length of 7");
+			return ResponseEntity.badRequest().build();}
 
 			//Encode Password before saving
 			user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 			userService.saveUser(user);
 			return ResponseEntity.ok(user);}
-	}
-	
+
 }
