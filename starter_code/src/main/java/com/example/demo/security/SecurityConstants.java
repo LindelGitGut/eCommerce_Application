7de6a1fa297378f877d0fc3bd53b14c559c5b011
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-05-03 10:23:15 +0200

    final commit for project

--- starter_code/src/main/java/com/example/demo/security/SecurityConstants.java
+++ starter_code/src/main/java/com/example/demo/security/SecurityConstants.java
@@ -2,9 +2,9 @@ package com.example.demo.security;
 
 public class SecurityConstants {
 
-    public static final String SECRET = "oursecretkey";
+    public static final String SECRET = "justanothersecret";
     public static final long EXPIRATION_TIME = 854_000_000;
-    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String TOKEN_PREFIX = "superDuperMegaSecret";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/user/create";
 
