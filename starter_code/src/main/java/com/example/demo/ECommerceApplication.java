7f3f49ca4afc72738733c5b6d66034ee19b7d4ee
LindelGitGut <alexandre.lindele@outlook.de>
2024-06-07 12:53:18 +0200

    added war compile

--- starter_code/src/main/java/com/example/demo/ECommerceApplication.java
+++ starter_code/src/main/java/com/example/demo/ECommerceApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -12,14 +14,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //exclude default Security Autoconfig
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class ECommerceApplication {
+public class ECommerceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ECommerceApplication.class);
+	}
 	@Bean
 	BCryptPasswordEncoder bCryptPasswordEncoder(){
 		return new BCryptPasswordEncoder();
